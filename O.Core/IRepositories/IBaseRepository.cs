7eba5b528aac515c7e46f56d9a68e45dca5a6c59
JAMEEL ALEDLBI <JAMEL_056@hotmail.com>
2021-03-27 10:40:36 +0300

    add Base Repository pattern

---
+++ O.Core/IRepositories/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace O.Core.IRepositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task AddAsync(TEntity entity);
+        void AddRangeAsync(IEnumerable<TEntity> entities);
+        Task<TEntity> GetAsync(int id);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task<int> SaveAsync();
+    }
+}
---
+++ O.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using O.Core.IRepositories;
+using O.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace O.Infrastructure.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        private readonly AppDbContext _context;
+
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+
+        public void AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().AddRange(entities);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        public async Task<TEntity> GetAsync(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await _context.Set<TEntity>().FirstOrDefaultAsync(expression);
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Update<TEntity>(entity);
+        }
+    }
+}
